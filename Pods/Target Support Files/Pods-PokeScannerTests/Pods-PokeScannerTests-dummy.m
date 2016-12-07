6c55131a4cf55754005dc8cf67a198e045c73f0e
Jordan Cech <jordancech@Jordans-MBP-2.casacisco.local>
2016-12-07 12:04:11 -0500

    Basic features of app complete.

---
+++ Pods/Target Support Files/Pods-PokeScannerTests/Pods-PokeScannerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PokeScannerTests : NSObject
+@end
+@implementation PodsDummy_Pods_PokeScannerTests
+@end
---
+++ PokeScanner/PokeScanner-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeScanner-Bridging-Header.h
+//  PokeScanner
+//
+//  Created by Jordan Cech on 12/1/16.
+//  Copyright © 2016 Strana LLC. All rights reserved.
+//
+
+#import "Geofire.h"
